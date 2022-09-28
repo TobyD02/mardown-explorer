7f31f24938adfc029d65874b34a0819a5aad602b
Toby Dixon <thetjdixon1234@gmail.com>
2022-09-28 10:16:03 +0100

    added recursive search. All .md files are found

--- main.py
+++ main.py
@@ -7,11 +7,31 @@ import os
 
 # Prints each file in directory
 def main(dir=os.getcwd()):
+    # Gather all markdown files
+
     res = []
     for path in os.listdir(dir):
-        if os.path.isfile(os.path.join(dir, path)):
+        if (os.path.isdir(os.path.join(dir, path))):
+            res = res + search_dir(os.path.join(dir, path), path)
+        if os.path.isfile(os.path.join(dir, path)) and path[-3:] == '.md':
             res.append(path)
-
     print(res)
 
+    # Read the files labelled .md
+    # for path in res:
+    #     if (path[-3:] == '.md'):
+    #         with open(path) as f:
+    #             print(f.read())
+
+def search_dir(dir, rel_dir):
+    res = []
+    for path in os.listdir(dir):
+        if os.path.isdir(os.path.join(dir, path)):
+            print(dir + '/' + path)
+            print('hit')
+            res = res + search_dir(os.path.join(dir, path), rel_dir + '/' + path)
+        if os.path.isfile(os.path.join(dir, path)) and path[-3:] == '.md':
+            res.append(rel_dir + '/' + path)
+
+    return res
 main()
